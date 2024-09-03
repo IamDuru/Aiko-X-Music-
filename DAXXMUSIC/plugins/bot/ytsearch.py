f3b94da6216e5b2c62d430e784c875ebe39f1fd6
OpAlex07 <durgeshgokhale656@gmail.com>
2024-09-03 13:35:39 +0530

    Update ytsearch.py

--- DAXXMUSIC/plugins/bot/ytsearch.py
+++ DAXXMUSIC/plugins/bot/ytsearch.py
@@ -9,7 +9,7 @@ from config import BOT_USERNAME
 
 
 
-@app.on_message(filters.command("search"))
+@app.on_message(filters.command(["yt", "youtube"], prefixes=["/", "!", "%", ",", "-", ".", "@", "#"]))
 async def ytsearch(_, message: Message):
     try:
         if len(message.command) < 2:
